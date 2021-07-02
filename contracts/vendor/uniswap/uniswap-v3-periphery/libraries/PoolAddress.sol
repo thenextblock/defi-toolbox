21fac7bce09792063c6067922191a6623f49845b
Vakho <vakho@tuta.io>
2021-07-02 16:32:00 +0400

    update POOL_INIT_CODE_HASH to match source

--- contracts/vendor/uniswap/uniswap-v3-periphery/libraries/PoolAddress.sol
+++ contracts/vendor/uniswap/uniswap-v3-periphery/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x4dd50b740ddd0bae5e0a18d02afe2ddda4f874449683856458ccd38547b38d8c;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
