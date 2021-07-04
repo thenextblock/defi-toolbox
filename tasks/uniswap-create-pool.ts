f678463523143b944dfe267fc1feee104ee1916d
Vakho <vakho@tuta.io>
2021-07-04 23:55:12 +0400

    add uniswap tasks: create pool, and mint (add liquidity )

--- tasks/uniswap-create-pool.ts
+++ tasks/uniswap-create-pool.ts
@@ -1,30 +1,94 @@
 import { task } from 'hardhat/config';
 import '@nomiclabs/hardhat-ethers/internal/type-extensions';
-import { NonfungiblePositionManager__factory, UniswapV3Factory__factory } from '../types';
-import { TickMath } from '@uniswap/v3-sdk';
+import {
+  NonfungiblePositionManager__factory,
+  TokenTemplate__factory,
+  UniswapV3Factory__factory,
+} from '../types';
+import {
+  encodeSqrtRatioX96,
+  FeeAmount,
+  nearestUsableTick,
+  TICK_SPACINGS,
+  TickMath,
+} from '@uniswap/v3-sdk';
 
 task('uniswap:create-pool', 'Create Uniswap V3 pool')
   .addParam('contract', 'NonfungiblePositionManager address')
   .addParam('token0', 'ERC2-20 token address')
   .addParam('token1', 'ERC2-20 token address')
-  .addParam('fee', '(300|500|3000)')
-  .addParam('price', 'token1/token0')
+  .addOptionalParam('fee', '(500|3000|10000)', FeeAmount.MEDIUM.toString())
+  .addParam('amount0')
+  .addParam('amount1')
   .setAction(async (args, hre) => {
+    const fee: FeeAmount = args.fee;
+    const ratio = encodeSqrtRatioX96(args.amount1, args.amount0);
+
     const [deployer] = await hre.ethers.getSigners();
-    const nonfungiblePositionManager = await NonfungiblePositionManager__factory.connect(
-      args.contract,
-      deployer
-    );
-    await nonfungiblePositionManager.createAndInitializePoolIfNecessary(
+    const nfpManager = await NonfungiblePositionManager__factory.connect(args.contract, deployer);
+
+    console.log(nfpManager.address);
+    const token0 = await TokenTemplate__factory.connect(args.token0, deployer);
+    const token1 = await TokenTemplate__factory.connect(args.token1, deployer);
+    const symbol0 = await token0.symbol();
+    const symbol1 = await token1.symbol();
+
+    console.log(`Create pool ${symbol0}/${symbol1}`);
+    await nfpManager.createAndInitializePoolIfNecessary(
       args.token0,
       args.token1,
-      args.fee,
-      TickMath.getSqrtRatioAtTick(parseFloat(args.price)).toString()
-    );
-    const uniswapV3Factory = await UniswapV3Factory__factory.connect(
-      await nonfungiblePositionManager.factory(),
-      deployer
+      fee,
+      ratio.toString()
     );
 
-    return await uniswapV3Factory!.getPool(args.token0, args.token1, args.fee);
+    const factory = await UniswapV3Factory__factory.connect(await nfpManager.factory(), deployer);
+    const poolAddress = await factory.getPool(args.token0, args.token1, fee);
+    console.log(poolAddress + '\n');
+
+    return poolAddress;
+  });
+
+task('uniswap:add-liquidity', 'Add liquidity to a pool')
+  .addParam('contract', 'NonfungiblePositionManager address')
+  .addParam('token0', 'ERC2-20 token address')
+  .addParam('token1', 'ERC2-20 token address')
+  .addOptionalParam('fee', '(500|3000|10000)', FeeAmount.MEDIUM.toString())
+  .addParam('amount0')
+  .addParam('amount1')
+  .addParam('amount0min', 'Minimum expected amount.')
+  .addParam('amount1min', 'Minimum expected amount.')
+  .addOptionalParam('deadline', 'Minutes. 1 min by default', '1')
+  .setAction(async (args, hre) => {
+    const fee: FeeAmount = args.fee;
+    const ratio = encodeSqrtRatioX96(args.amount1, args.amount0);
+    const tick = TickMath.getTickAtSqrtRatio(ratio);
+    const tickSpacing = TICK_SPACINGS[fee];
+    const deadline = Math.ceil(Date.now() / 1000) + 60 * parseInt(args.deadline, 10);
+
+    const [deployer] = await hre.ethers.getSigners();
+    const nfpManager = await NonfungiblePositionManager__factory.connect(args.contract, deployer);
+    console.log(`uniswap:add-liquidity > nfpManager: ${nfpManager.address}`);
+    const token0 = await TokenTemplate__factory.connect(args.token0, deployer);
+    const token1 = await TokenTemplate__factory.connect(args.token1, deployer);
+
+    console.log('Approve spender to transfer money');
+    await token0.approve(nfpManager.address, args.amount0);
+    await token1.approve(nfpManager.address, args.amount1);
+
+    console.log('Add liquidity');
+    const mintParams = {
+      token0: args.token0,
+      token1: args.token1,
+      fee: fee,
+      tickLower: nearestUsableTick(tick, tickSpacing) - tickSpacing,
+      tickUpper: nearestUsableTick(tick, tickSpacing) + tickSpacing,
+      amount0Desired: args.amount0,
+      amount1Desired: args.amount1,
+      amount0Min: args.amount0min,
+      amount1Min: args.amount1min,
+      recipient: deployer.address,
+      deadline: deadline,
+    };
+    console.log('params:', mintParams);
+    return await nfpManager.mint(mintParams);
   });
