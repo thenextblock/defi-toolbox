ad38c085c5f9244959dbcaefb27682e9642f63d4
Vakho <vakho@tuta.io>
2021-10-09 19:28:09 +0400

    add export Erc20Token__factory type to hardhat-erc20

--- packages/hardhat-erc20/src/index.ts
+++ packages/hardhat-erc20/src/index.ts
@@ -1,3 +1,4 @@
 export { Erc20Token } from '../typechain';
+export { Erc20Token__factory } from '../typechain/factories/Erc20Token__factory';
 export { deployErc20Token } from './functions';
 export { ERC20_DEPLOY, ERC20_MINT } from './tasks';
