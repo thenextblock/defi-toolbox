696dc18da0923820d07e7637e0cd662a04fa61fa
Vakho <vakho@tuta.io>
2021-10-09 19:31:48 +0400

    remove leftover from harhdat-uniswap-v3

--- packages/hardhat-uniswap-v3/hardhat.config.ts
+++ packages/hardhat-uniswap-v3/hardhat.config.ts
@@ -12,52 +12,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.6.12',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
         version: '0.7.3',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.7.5',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.7.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.5.16',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.4.18',
         settings: {
           optimizer: {
             enabled: true,
